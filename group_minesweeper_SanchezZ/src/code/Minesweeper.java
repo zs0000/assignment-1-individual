4425a181484a15773110be4a103405247579f28d
zs0000 <84543723+zs0000@users.noreply.github.com>
2024-10-07 17:55:47 -0700

    fix

--- group_minesweeper_SanchezZ/src/code/Minesweeper.java
+++ group_minesweeper_SanchezZ/src/code/Minesweeper.java
@@ -7,7 +7,6 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
-import java.util.TreeMap;
 
 public class Minesweeper {
 	//https://stackoverflow.com/questions/6802483/how-to-directly-initialize-a-hashmap-in-a-literal-way
@@ -27,8 +26,7 @@ public class Minesweeper {
 
 	private int myRows;
 	private int myCols;
-	
-
+	private int myCount;
 	
 	public Minesweeper() {
 		// TODO Auto-generated constructor stub
@@ -37,18 +35,17 @@ public class Minesweeper {
 	public static void main(String[] args) {
 		
 		Minesweeper testBoard = new Minesweeper();
-		testBoard.playMinesweeper();
-		testBoard.getBoardsCountFromFile();
+		//testBoard.playMinesweeper();
+		
+		//uncomment these to test from input file.
+		//testBoard.getBoardsCountFromFile();
 		testBoard.loadAllBoards();
-//		testBoard.loadFirstBoard();
-//		testBoard.produceHintsBoard(testBoard.myBoard);
-//		testBoard.printHintsBoard();
 
 	}
 
 	public final void saveBoard(char[][] board, String boardType) {
 		Map<String, String> keyMap = Map.of(
-				"hints","minesweeper_output.txt",
+				"hints","my_minesweeper_output.txt",
 				"mines","minesweeper_input.txt"
 				);
 		 try {
@@ -59,7 +56,8 @@ public class Minesweeper {
 	            
 	            int cols = this.getCols();
 	            int rows = this.getRows();
-	            writer.write(rows + " " + cols	+ "\n");
+	            int count = this.getCount();
+	            writer.write("Field #" + count +":"	+ "\n");
 	            
 	            for (int i = 0; i < rows; i++) {
 	                for (int j = 0; j < cols; j++) {
@@ -67,7 +65,7 @@ public class Minesweeper {
 	                }
 	                writer.write("\n");
 	            }
-	            writer.write("0 0\n");
+	            writer.write("\n");
 
 	            writer.close();
 	        } catch (IOException e) {
@@ -105,64 +103,22 @@ public class Minesweeper {
 		
 		this.saveBoard(currHintsBoard, "hints");
 		this.saveBoard(field, "mines");
-		
+		scanner.close();
 	}
-	
-	private final void loadFirstBoard(){
-		try {
-			File file = new File("minesweeper_input.txt");
-			Scanner scanner = new Scanner(file);
-			String[] rowsAndColsAsStr = scanner.nextLine().split(" ");
-			int boardRows = Integer.parseInt(rowsAndColsAsStr[0]);
-			int boardCols= Integer.parseInt(rowsAndColsAsStr[1]);
-			if(this.inRange(boardRows) && this.inRange(boardCols)) {
-				setRows(boardRows);
-				setCols(boardCols);
-			}
-			int currRow = 0;
-			int currCol = 0;
-			char[][] board = new char[boardRows][boardCols];
-			while (scanner.hasNextLine()) {
-				String currLine = scanner.nextLine();
-				String[] checkString = currLine.split(" ");	
-				if(checkString.length == 2) {
-					//skip, as this is a start or end.
-				} else {
-					for(char currCell: currLine.toCharArray()) {
-						board[currRow][currCol] = currCell;
-						currCol++;
-					}
-					currRow++;
-					currCol = 0;
-				}
-					
-				
-				//end cond
-				if(checkString.length == 2 
-				&& checkString[0].equals("0") 
-				&& checkString[1].equals("0")
-				) {
-					this.setBoard(board);
-					this.printBoard();
-					break;
-				}
-			}
-			scanner.close();
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		}
-	}
-	
 	
 	// largely inspired by https://www.digitalocean.com/community/tutorials/java-read-file-line-by-line
 	private final void loadAllBoards() {
 
 		try {
+			
+			
+			
 			Scanner scanner = new Scanner(new File("minesweeper_input.txt"));
+			int boardCount = 1;
 			int rowsCount = 0;
 			int colsCount = 0;
 			int rowIdx = 0;
-
+			this.setCount(boardCount);
 			char[][] board = new char[1][1];
 			while(scanner.hasNextLine()) {
 				String currLine = scanner.nextLine();
@@ -174,15 +130,22 @@ public class Minesweeper {
 					this.setCols(colsCount);
 					board = new char[rowsCount][colsCount];
 				} else {
-					if(currLine.contains(" 0")) {
-						rowIdx = 0;
-						rowsCount = 0;
-						colsCount = 0;
+					if(!currLine.contains("*") && !currLine.contains(".")) {
+						
 						this.setBoard(board);
+						char[][] currHintBoard = this.produceHintsBoard(board);
 						this.printBoard();
-						this.produceHintsBoard(board);
 						this.printHintsBoard();
-
+						
+						this.saveBoard(currHintBoard, "hints");
+						rowsCount = Integer.parseInt(tempStrs[0]);
+						colsCount = Integer.parseInt(tempStrs[1]);
+						this.setRows(rowsCount);
+						this.setCols(colsCount);
+						board = new char[rowsCount][colsCount];
+						rowIdx=0;
+						this.setCount(++boardCount);
+						
 					} else {
 						board[rowIdx] = currLine.toCharArray();
 						rowIdx++;
@@ -280,6 +243,10 @@ public class Minesweeper {
 		return Integer.valueOf(this.myCols);
 	}
 	
+	private final int getCount() {
+		return Integer.valueOf(this.myCount);
+	}
+	
 	private final void setBoard(final char[][] theBoard) {
 		if(!theBoard.equals(null)) {
 			this.myBoard = theBoard;
@@ -301,6 +268,12 @@ public class Minesweeper {
 	private final void setCols(final int theCols) {
 		if(this.inRange(theCols)) {
 			this.myCols = theCols;
+		}
+	}
+	
+	private final void setCount(final int theCount) {
+		if(theCount > 0) {
+			this.myCount = theCount;
 		}
 	}
 
